59dc206159cd4a3bee989e634014a0c142148cc6
David <ledavid64@gmail.com>
2018-04-20 18:37:04 -0700

    Add state

--- src/App.js
+++ src/App.js
@@ -1,70 +1,84 @@
 import React, { Component } from "react";
 import "./App.css";
 import propTypes from "prop-types";
+import shuffle from "lodash.shuffle";
 
 const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-const HIDDEN_SYMBOL = "__";
+const PHRASE = [
+  "BONJOUR",
+  "SOLEIL",
+  "BONHEUR",
+  "SATURNE",
+  "TABLE",
+  "SUPER",
+  "OCEAN",
+  "Si debugger, c’est supprimer des bugs, alors programmer ne peut être que les ajouter "
+];
 
 const GuessCount = ({ guesses }) => <div className="guesses">{guesses}</div>;
-
-GuessCount.propTypes = {
-  guesses: propTypes.number.isRequired
-};
-
-const GuessWord = ({ letter, feedback }) => (
-  <div className="guessletter">
-    <span className={`letter ${feedback}`}>
-      {feedback === "hidden" ? HIDDEN_SYMBOL : letter}
+const GuessPhrase = ({ phrase }) => (
+  <div className="guessphrase">
+    <span className="phrase">
+      {phrase}
     </span>
   </div>
 );
-
-GuessWord.propTypes = {
-  letter: propTypes.string.isRequired,
-  feedback: propTypes.oneOf([
-    'visible',
-    'hidden'
-  ]).isRequired
-};
-
 const Keyboard = ({ letter, onClick }) => (
   <div className="letter" letter={letter} onClick={() => onClick(letter)}>
     {letter}
   </div>
 );
+const won = true;
+const usedLetters = new Set([]);
+
+// ### propTypes ###
+
+GuessCount.propTypes = {
+  guesses: propTypes.number.isRequired
+};
+
+GuessPhrase.propTypes = {
+  phrase: propTypes.string.isRequired,
+}
 
 Keyboard.propTypes = {
   letter: propTypes.string.isRequired,
-  onClick: propTypes.func.isRequired,
-}
-
-const won = true;
+  onClick: propTypes.func.isRequired
+};
 
 class App extends Component {
-
-  keyboard = ALPHABET.split('');
+  state = {
+    keyboard: ALPHABET.split(""),
+    phrase: shuffle(PHRASE).pop().toUpperCase(),
+  };
 
   handleLetterClick(letter) {
-    console.log(letter, "clicked");
+    usedLetters.add(letter)
+    console.log(usedLetters);    
+  }
+
+  // Raffraichir le masque pour afficher les lettres trouvé !!
+  computeDisplay(phrase, letter) {
+    console.log(phrase);
+    return phrase.replace(/\w/g,
+      (letter) => (usedLetters.has(letter) ? letter : '_')
+    )
   }
 
   render() {
+    const { keyboard, phrase } = this.state;
     return (
       <div className="hangman">
         <GuessCount guesses={0} />
 
-        <div className="guessword">
-          <GuessWord letter="S" feedback="hidden" />
-          <GuessWord letter="O" feedback="visible" />
-          <GuessWord letter="L" feedback="hidden" />
-          <GuessWord letter="E" feedback="visible" />
-          <GuessWord letter="I" feedback="hidden" />
-          <GuessWord letter="L" feedback="visible" />
+        <div className="guessphrase">
+          <GuessPhrase
+            phrase={this.computeDisplay(phrase, usedLetters)}
+          />
         </div>
 
         <div className="keyboard">
-          {this.keyboard.map((letter, index) => (
+          {keyboard.map((letter, index) => (
             <Keyboard
               letter={letter}
               onClick={this.handleLetterClick}
@@ -80,15 +94,3 @@ class App extends Component {
 }
 
 export default App;
-
-// const set1 = new Set([1, 2, 3, 4, 5]);
-
-// Produit une représentation textuelle de l’état de la partie,
-// chaque lettre non découverte étant représentée par un _underscore_.
-// (CSS assurera de l’espacement entre les lettres pour mieux
-// visualiser le tout).
-// computeDisplay(phrase, usedLetters) {
-//   return phrase.replace(/\w/g,
-//     (letter) => (usedLetters.has(letter) ? letter : '_')
-//   )
-// }
