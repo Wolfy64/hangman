e166d7d809edf9bd361d2b92eed98b2fb8392d91
David De Wulf <contact.dewulf@gmail.com>
2018-11-12 00:52:58 -0800

    Refactor App

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import './styles/appStyle.css';
 
 import GuessCount from './components/GuessCount/GuessCount';
 import GuessWord from './components/GuessWord/GuessWord';
-import Letter from './components/Letter/Letter';
 import Keyboard from './components/Keyboard/Keyboard';
 import Result from './components/Result/Result';
 
-// Constant
+/* Constant */
 const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 const WORD_LIST = [
   'BONJOUR',
@@ -46,7 +45,9 @@ class App extends React.Component {
       .toUpperCase();
   }
 
-  handleLetter(letter) {
+  /* Add Letter in "letterSet" state 
+    OR decrement "attempt" state */
+  handleClickLetter(letter) {
     let { attempt, lettersSet, wordToGuess } = this.state;
     const isNotClicked = !lettersSet.has(letter);
     const isNotMatchWord = !wordToGuess.split('').includes(letter);
@@ -59,15 +60,24 @@ class App extends React.Component {
     });
   }
 
+  /* If True return <Result> */
   handleResult() {
     const { attempt, wordToGuess } = this.state;
-    if (attempt === 0) return <Result clicked={this.newGame} />;
-    if (this.handleHiddenWord() === wordToGuess)
-      return <Result success clicked={this.newGame} />;
+    const endGame = attempt === 0 || this.handleHiddenWord() === wordToGuess;
+
+    return endGame ? (
+      <Result clicked={this.newGame} success={attempt !== 0} />
+    ) : (
+      false
+    );
   }
 
+  /* To replace word's letter by space 
+    if not match lettersSet state. 
+    Return String */
   handleHiddenWord() {
     const { lettersSet, wordToGuess } = this.state;
+
     return wordToGuess.replace(/\w/g, letter =>
       lettersSet.has(letter) ? letter : ' '
     );
@@ -75,21 +85,19 @@ class App extends React.Component {
 
   render() {
     const { attempt, lettersSet } = this.state;
-    const letters = ALPHABET.map((letter, index) => (
-      <Letter
-        key={index}
-        letter={letter}
-        cover={lettersSet.has(letter)}
-        clicked={() => this.handleLetter(letter)}
-      />
-    ));
 
     return (
-      <div className="hangman">
+      <div className='hangman'>
         <h1> Welcome to the Hangman Game </h1>
         <GuessCount guesses={attempt} />
         <GuessWord hiddenWord={this.handleHiddenWord()} />
-        {this.handleResult() || <Keyboard letters={letters} />}
+        {this.handleResult() || (
+          <Keyboard
+            letters={ALPHABET}
+            cover={letter => lettersSet.has(letter)}
+            clicked={letter => this.handleClickLetter(letter)}
+          />
+        )}
       </div>
     );
   }
@@ -97,7 +105,7 @@ class App extends React.Component {
 
 export default App;
 
-// PropTypes
+/* PropTypes */
 GuessCount.propTypes = {
   guesses: propTypes.number.isRequired
 };
@@ -106,10 +114,8 @@ GuessWord.propTypes = {
   hiddenWord: propTypes.string.isRequired
 };
 
-Letter.propTypes = {
-  cover: propTypes.bool.isRequired
-};
-
 Keyboard.propTypes = {
-  letters: propTypes.array.isRequired
+  letters: propTypes.array.isRequired,
+  cover: propTypes.func.isRequired,
+  clicked: propTypes.func.isRequired
 };
--- src/components/GuessCount/GuessCount.js
+++ src/components/GuessCount/GuessCount.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SMILEYS = ['😵', '😱', '😰', '😓', '😦', '🙄', '🤔', '🙂'];
 
 const GuessCount = ({ guesses }) => (
-  <div className="guesses">
+  <div className='guesses'>
     {SMILEYS.map((el, index) => (
       <span key={index} className={index === guesses ? 'active' : null}>
         {el}
