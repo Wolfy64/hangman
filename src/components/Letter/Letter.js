bc49a4974ec7f6a08f714cbe2b6b05460f1f7325
David De Wulf <contact.dewulf@gmail.com>
2018-11-12 08:43:20 -0800

    Remove useless props

--- src/components/Letter/Letter.js
+++ src/components/Letter/Letter.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Letter = props => (
   <div
     className={`letter${props.cover ? ' cover' : ''}`}
-    feedback={props.getFeedbackForLetter}
     onClick={() => props.clicked()}>
     {props.letter}
   </div>
