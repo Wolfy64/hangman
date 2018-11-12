3f56172437efed16f33c68cae1ea133ae8853ea3
David De Wulf <contact.dewulf@gmail.com>
2018-11-12 11:35:02 -0800

    Refactor Result Component

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Result = props => (
   <div>
-    <h2>{props.success ? '🎉🎉 Victoire ! 🎉🎉' : '☠️☠️ Perdu ! ☠️ ☠️'}</h2>
+    <div className='result'>
+      {props.success ? '🥳 You Rock ! 🤘' : 'You Passed Away... 🧟‍♂️'}
+    </div>
     <button onClick={() => props.clicked()}>Start</button>
   </div>
 );
