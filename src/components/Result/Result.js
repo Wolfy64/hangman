a790a3b971eb13b97561ad682efa537b3057b44d
David De Wulf <contact.dewulf@gmail.com>
2018-11-13 15:47:12 -0800

    Add Styled on Result Cmponent

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 const Result = props => (
-  <div>
-    <div className='result'>
-      {props.success ? '🥳 You Rock ! 🤘' : 'You Passed Away... 🧟‍♂️'}
-    </div>
-    <button onClick={() => props.clicked()}>Start</button>
+  <div className='result'>
+    <p>{props.success ? '🥳 You Rock ! 🤘' : 'You Passed Away... 🧟‍♂️'}</p>
+    <button onClick={() => props.clicked()}>Play</button>
   </div>
 );
 
